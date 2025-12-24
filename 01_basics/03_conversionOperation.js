64fa343e5194348bfe5db09f73b4c1f167012069
Your Name <your-email@example.com>
2025-12-24 22:35:44 +0530

    Daily learning update: Dec 24, 2025

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,21 @@ console.log(number); //NaN
 //NaN => not a number
 //isNaN() => to check if value is NaN
 console.log(isNaN(number)); //true
+
+let string1 = null;
+console.log(typeof string1); //string
+let number1 = Number(string1); //explicit conversion
+console.log(typeof number1); //number
+console.log(number1); //0
+
+let boolean = true;
+console.log(typeof boolean); //boolean
+let number2 = Number(boolean); //explicit conversion
+console.log(typeof number2); //number
+console.log(number2); //1
+
+let string3 = undefined;
+console.log(typeof string3); //undefined
+let number3 = Number(string3); //explicit conversion
+console.log(typeof number3); //number
+console.log(number3); //NaN
