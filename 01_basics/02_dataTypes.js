f209d39b5b0cabcb9d8e5b87decbdefb94812901
Your Name <your-email@example.com>
2025-12-24 21:36:47 +0530

    Daily learning update: Dec 24, 2025

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//  alert("Hello Aditya"); similar to console.log but creates a popup
+
+let name = "Aditya";
+let age = 21;
+let isApproved = false;
+
+//number => 2 to pow 53
+//bigint => larger than 2 to pow 53
+//string => '', "", ``
+//boolean => true/false
+//undefined => value not assigned
+//null => empty value
+//symbol => unique identifiers
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof null); //bug in JS, returns object instead of null
+console.log(typeof undefined);
+// console.table([name, age, isApproved]);
