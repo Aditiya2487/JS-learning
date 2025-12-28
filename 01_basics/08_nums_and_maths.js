f8f5874faa19f81bf27a8ab7226f53adcc746c0d
Aditya2487 <73511460+Aditya2487@users.noreply.github.com>
2025-12-28 22:24:57 +0530

    Daily learning update: Dec 28, 2025

--- 01_basics/08_nums_and_maths.js
+++ 01_basics/08_nums_and_maths.js
@@ -12,7 +12,30 @@ console.log(balance.toPrecision(3)); //"1.00e+3" //Formats number to specified l
 // toPrecision is used for significant digits representation like in scientific notation ex:- 1.23e+5 for 123000 and 123.9 for 123.89 and so on
 
 console.log(Number.isInteger(score)); //true //Checks if score is an integer
-console.log(Number.isNaN(NaN)); //true //Checks if value is NaN
+console.log(Number.isNaN(NaN)); //true //Checks if value is NaN. It is a object method
 
 const hundreds = 10000000;
 console.log(hundreds.toLocaleString("en-IN")); //"1,00,00,000" //Formats number according to locale (Indian Numbering System)   if you use "en-US" it will format as "10,000,000"
+
+//********************* MATHS ********************  */
+console.log(Math);
+console.log(Math.PI); //3.141592653589793
+console.log(Math.E); //2.718281828459045
+console.log(Math.SQRT2); //1.4142135623730951
+console.log(Math.abs(-55)); //55  //Absolute value
+console.log(Math.round(4.6)); //5  //Rounds to nearest integer
+console.log(Math.floor(4.9)); //4  //Rounds down to nearest integer
+console.log(Math.ceil(4.1)); //5  //Rounds up to nearest integer
+console.log(Math.min(0, 150, 30, 20, -8, -200)); //-200  //Returns minimum value
+console.log(Math.max(0, 150, 30, 20, -8, -200)); //150  //Returns maximum value
+console.log(Math.random()); //Random number between 0 and 1
+console.log(Math.random() * 100); //Random number between 0 and 100
+console.log(Math.floor(Math.random() * 100)); //Random integer between 0 and 99
+console.log(Math.floor(Math.random() * 100) + 1); //Random integer between 1 and 100
+console.log(Math.pow(2, 3)); //8  //2 raised to the power 3
+console.log(Math.sqrt(64)); //8  //Square root of 64
+console.log(Math.cbrt(27)); //3  //Cube root of 27
+console.log(Math.log(1)); //0  //Natural logarithm of 1
+console.log(Math.log10(1000)); //3  //Base 10 logarithm of 1000
+console.log(Math.sin(Math.PI / 2)); //1  //Sine of 90 degrees (PI/2 radians)
+console.log(Math.cos(0)); //1  //Cosine of 0 degrees
