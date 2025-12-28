f1ee46cd10777c38437f57dd449f8385cb3111e4
Aditya2487 <73511460+Aditya2487@users.noreply.github.com>
2025-12-28 13:28:32 +0530

    Daily learning update: Dec 28, 2025

---
+++ 01_basics/07_strings.js
@@ -0,0 +1,14 @@
+const name = "Aditya";
+const repoCount = 50;
+
+console.log("Hello " + name + " , your repo count is " + repoCount);
+console.log(`Hello ${name} , your repo count is ${repoCount}`); //String Interpolation (Modern Way  to include variables in strings using backticks)
+const gameName = new String("Chess"); //String Object (Not recommended to use)
+console.log(gameName);
+console.log(typeof gameName); //object
+
+// String Properties and Methods
+console.log(gameName[0]); //Accessing first character 'C'
+console.log(gameName.__proto__); //Prototype of String Object
+
+console.log(gameName.length); //Length of the string
